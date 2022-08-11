3cac700dc5f80ca05b2db3b377944d106adca49b
ankushk1729 <ankushrattan17@gmail.com>
2022-08-11 16:16:43 +0530

    update model

---
+++ pages/api/todo/index.ts
@@ -0,0 +1,28 @@
+
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { prisma } from '../../../lib/prisma'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+
+  if(req.method === 'GET'){
+    const todos = await prisma.todo.findMany({})
+    res.status(200).json({ todos })  
+    return
+  }
+
+  if(req.method === 'POST'){
+    const { text } = req.body
+    const new_todo = await prisma.todo.create({
+      data:{
+        text 
+      }
+    })
+    res.status(201).json({todo:new_todo})
+    return
+  }
+
+  res.status(200).json({ name: 'John Doe' })
+}
