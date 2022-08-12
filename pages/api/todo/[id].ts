2c4d1a9394f1f548025818b3477f40739e409b42
ankushk1729 <ankushrattan17@gmail.com>
2022-08-12 12:17:58 +0530

    Add get and delete todo requests

--- pages/api/todo/[id].ts
+++ pages/api/todo/[id].ts
@@ -2,8 +2,31 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { prisma } from '../../../lib/prisma';
 
 export default async function handler(req: NextApiRequest, res:NextApiResponse) {
-    const { id, text } = req.body
+    const { id } = req.query
+    console.log(id)
+
+    if(!id || typeof(id) != 'string'){
+        res.status(400).json({msg: 'Please provide a valid id'})
+        return
+    }
+
+    if(req.method === 'GET'){
+        const todo = await prisma.todo.findFirst({
+            where:{
+                id
+            }
+        })
+        if(!todo){
+            res.status(404).json({msg: `No todo with id ${id}`})
+            return
+        } 
+
+        res.status(200).json({todo})
+        return
+    }
+
     if(req.method === 'PATCH'){
+        const { text } = req.body
         const updated_todo = await prisma.todo.update({
             where:{
                 id
