3d15dca3d96785cadb15cf2614f4a23939e86312
ankushk1729 <ankushrattan17@gmail.com>
2022-08-13 20:25:34 +0530

    Remove env :)

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -4,7 +4,6 @@ import { Button } from '../pages';
 
 
 const fetchTodos = async (): Promise<Todo[]> => {
-  console.log(process.env.NEXT_PUBLIC_API_URL)
   const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/todo`)
   return await res.json();
 }
